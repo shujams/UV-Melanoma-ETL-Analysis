37884f9137839e6fe503c31b79e76b1033a289a7
Michelle Handipranata <michellehandipranata@Michelles-MacBook-Pro.local>
2020-03-29 19:09:09 -0700

    Create uv_melanoma_ETL.py file which is created based on jupyter notebook. Modified and added the routes on app.py to upload data to pymongo and have our own api that includes all the data which we will use for js (visualizations). Started on index.html and js

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our scatter chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.json("/api/get_all_data").then(function(){
+    console.log
+})
